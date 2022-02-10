13a099f2c2cb0ad5f3fdf661aeb4d24877233035
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-10 11:36:13 +0900

    Create Restaurant

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
 export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant)
-    private readonly restaurants: Repository<Restaurant>,
+    private readonly restaurantRepository: Repository<Restaurant>,
   ) {}
   getAll(): Promise<Restaurant[]> {
-    return this.restaurants.find();
+    return this.restaurantRepository.find();
+  }
+
+  createRestaurant(
+    createRestaurantDto: CreateRestaurantDto,
+  ): Promise<Restaurant> {
+    const newRestaurant = this.restaurantRepository.create(createRestaurantDto);
+    return this.restaurantRepository.save(newRestaurant);
   }
 }
