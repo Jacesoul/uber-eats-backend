e69ec3fcdd5ca13bae5529f12f5d5b17fa7d6e99
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-10 13:14:12 +0900

    Create Account Mutation part One

---
+++ src/users/dtos/create-account.dto.ts
@@ -0,0 +1,18 @@
+import { Field, ObjectType, PickType, InputType } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+
+@InputType()
+export class CreateAccountInput extends PickType(User, [
+  'email',
+  'password',
+  'role',
+]) {}
+
+@ObjectType()
+export class CreateAccountOutput {
+  @Field((type) => String, { nullable: true })
+  error?: string;
+
+  @Field((type) => Boolean)
+  ok: boolean;
+}
