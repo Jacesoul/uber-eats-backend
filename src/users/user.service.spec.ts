b2a689a1b1bcbc61ce5562d4e7e7b5573d3a2c4e
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-17 14:04:53 +0900

    type MockRepository & TypeScript Partial/Record

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { UserService } from './user.service';
@@ -21,8 +22,11 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const modules = await Test.createTestingModule({
@@ -47,13 +51,17 @@ describe('UserService', () => {
       ],
     }).compile();
     service = modules.get<UserService>(UserService);
+    userRepository = modules.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    it('should fail if user exists', () => {});
+  });
+
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
