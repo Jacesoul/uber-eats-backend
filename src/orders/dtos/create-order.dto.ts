96dd7a38ba7bfac3a5b65326a2beedb417eeddbd
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-20 18:16:48 +0900

    Create Order part One

---
+++ src/orders/dtos/create-order.dto.ts
@@ -0,0 +1,12 @@
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { Order } from '../entities/order.entity';
+
+@InputType()
+export class CreateOrderInput extends PickType(Order, ['dishes']) {
+  @Field((type) => Number)
+  restaurantId: number;
+}
+
+@ObjectType()
+export class CreateOrderOutput extends CoreOutput {}
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,4 +1,11 @@
+import { OrderResolver } from './order.resolver';
+import { OrderService } from './order.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { Order } from './entities/order.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Order])],
+  providers: [OrderService, OrderResolver],
+})
 export class OrdersModule {}
