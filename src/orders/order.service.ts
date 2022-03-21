12ec084bbf5e5133b29a000e357604292b5bbdbf
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-21 17:15:39 +0900

    Create Order part Five

--- src/orders/order.service.ts
+++ src/orders/order.service.ts
@@ -32,18 +32,41 @@ export class OrderService {
         error: 'Restaurant not found',
       };
     }
-    items.forEach(async (item) => {
+    for (const item of items) {
       const dish = await this.dishRepository.findOne(item.dishId);
       if (!dish) {
-        // abort this whole thing
+        return {
+          ok: false,
+          error: 'Dish not found',
+        };
       }
-      await this.orderItemRepository.save(
-        this.orderItemRepository.create({
-          dish,
-          options: item.options,
-        }),
-      );
-    });
+      console.log(`Dish price : ${dish.price}`);
+      for (const itemOption of item.options) {
+        const dishOption = dish.options.find(
+          (dishOption) => dishOption.name === itemOption.name,
+        );
+        if (dishOption) {
+          if (dishOption.extra) {
+            console.log(`$USD + ${dishOption.extra}`);
+          } else {
+            const dishOptionChoice = dishOption.choices.find(
+              (optionChoice) => optionChoice.name === itemOption.choice,
+            );
+            if (dishOptionChoice) {
+              if (dishOptionChoice.extra) {
+                console.log(`$USD + ${dishOptionChoice.extra}`);
+              }
+            }
+          }
+        }
+      }
+      // await this.orderItemRepository.save(
+      //   this.orderItemRepository.create({
+      //     dish,
+      //     options: item.options,
+      //   }),
+      // );
+    }
     // const order = await this.orderRepository.save(
     //   this.orderRepository.create({ customer, restaurant }),
     // );
