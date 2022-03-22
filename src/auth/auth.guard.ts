4a9af7830be2475eb2d2f960177450c6293c5921
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-22 14:19:54 +0900

    Subscription Authentication part Two

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,13 +2,19 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { gql } from 'apollo-server-express';
+import { JwtService } from 'src/jwt/jwt.service';
 import { User } from 'src/users/entities/user.entity';
+import { UserService } from 'src/users/user.service';
 import { AllowedRoles } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext) {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
       context.getHandler(),
@@ -17,18 +23,26 @@ export class AuthGuard implements CanActivate {
       return true;
     }
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    if (gqlContext.req) {
-      console.log(gqlContext.req.headers['x-jwt']);
+    const token = gqlContext.req
+      ? gqlContext.req.headers['x-jwt']
+      : gqlContext['X-JWT'];
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+        if (!user) {
+          return false;
+        }
+        gqlContext['user'] = user;
+        if (roles.includes('Any')) {
+          return true;
+        }
+        return roles.includes(user.role);
+      } else {
+        return false;
+      }
     } else {
-      console.log(gqlContext['X-JWT']);
-    }
-    const user: User = gqlContext['user'];
-    if (!user) {
       return false;
     }
-    if (roles.includes('Any')) {
-      return true;
-    }
-    return roles.includes(user.role);
   }
 }
